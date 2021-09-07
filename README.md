fa038333d7bf3529c16b9bcdaa1cbc90491ceea6
Lucas Amaral <lucas@pop-os.localdomain>
2021-09-07 14:01:13 -0300

    Finish JWT auth

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This repo was created to learn how to use JWT auth in Rails.
 
+### [Medium post](https://medium.com/binar-academy/rails-api-jwt-authentication-a04503ea3248)
+
+
 * install dependencies
 ```sh
     $ bundle install
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, param: :_username
+  post '/signin', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
